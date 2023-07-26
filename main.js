2b0adffa72e4d2f143bc9b3b201e402ca340a725
Seungjae Lim <seungjaelim@kaist.ac.kr>
2023-07-26 16:07:26 +0900

    2. Load image

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ ctx = canvas.getContext("2d")
 canvas.width=400;
 canvas.height=700;
 document.body.appendChild(canvas)
+
+let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
+
+function loadImage(){
+    backgroundImage = new Image();
+    backgroundImage.src="images/background.jpg";
+
+    spaceshipImage = new Image();
+    spaceshipImage.src = "images/spaceship.png"
+
+    bulletImage = new Image();
+    bulletImage.src = "images/bullet.png"
+
+    enemyImage = new Image();
+    enemyImage.src = "images/enemy.png"
+
+    gameOverImage = new Image();
+    gameOverImage.src = "images/gameover.png"
+
+
+}
